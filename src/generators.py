ae4a79bfbc996192704c1b3ccff09f66f30c49fa
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-10-02 01:11:06 -0400

    Update modular

--- src/generators.py
+++ src/generators.py
@@ -10,9 +10,9 @@ def is_generator(g, n):
     for p in range(0, n - 1):
         pows.append(g ** p % n)
     for m in range(1, n):
-        if finish: break
         if not pows.__contains__(m):
             finish = True
+            break
     return not finish
 
 # return list of generators of 𝐙/n𝐙
@@ -27,19 +27,18 @@ def generators(n):
 # otherwise, raise a ValueError
 def random_gen(n):
     gens = generators(n)
-    if not gens:
-      raise ValueError(f'No multiplicative generators of 𝐙/{n}𝐙')
+    if not gens: return None
     else:
       i = random.randint(0, len(gens) - 1)
       return gens[i]
 
 # compute the discrete log, if it exists
-# otherwise, raise a ValueError
+# otherwise, return None
 # x = base
 # y = target
 # n = modulus
 def discrete_log(x, y, n):
-    finish = False
+    finish = False # early exit
     res = None
     x = x % n
     y = y % n
@@ -48,15 +47,21 @@ def discrete_log(x, y, n):
         elif (x ** p) % n == y:
             finish = True
             res = p
-    if finish:
-        return res
-    else:
-        raise ValueError(f'{y} does not have a discrete log base {x} modulo {n}')
+    return res
 
 # check discrete logarithm
 # x = base
 # p = exponent
 # y = target
 # n = modulus
-def check_discrete_log(x, p, y, n):
+def check_discrete_log(p, x, y, n):
     return (x ** p) % n == y
+
+# make functions for specific value of n
+def make(n):
+    is_gen = lambda g: is_generator(g, n)
+    gens = lambda: generators(n)
+    rand_gen = lambda: random_gen(n)
+    dlog = lambda x, y: discrete_log(x, y, n)
+    check_dlog = lambda p, x, y: check_discrete_log(p, x, y, n)
+    return is_gen, gens, rand_gen, dlog, check_dlog
