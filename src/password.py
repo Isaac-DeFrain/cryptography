3ca13ccbce4cc4b5936ab7ec70f353f515792b69
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-10-01 21:59:09 -0400

    Add simple password code

---
+++ src/password.py
@@ -0,0 +1,30 @@
+#!/usr/bin python3
+
+from getpass import getpass
+from hashlib import pbkdf2_hmac
+from os import urandom
+
+# always salt your hash
+salt = urandom(32)
+hash = lambda x: pbkdf2_hmac('sha256', x, salt, 100_000)
+
+# get a confirmed password within 5 attempts
+attempts = 5
+while attempts >= 0:
+    p = hash(bytes(getpass('Password: '), encoding='utf8'))
+    q = hash(bytes(getpass('Confirm: '), encoding='utf8'))
+    if p != q:
+        if attempts == 1:
+            print('Password mismatch. Try again. You have %s remaining attempt.' % attempts)
+        elif attempts > 0:
+            print('Password mismatch. Try again. You have %s remaining attempts.' % attempts)
+        else:
+            print('Sorry for your luck. Bye.')
+        attempts -= 1
+    else:
+        print('Your password is: ' + p.hex())
+        break
+
+def guess():
+    g = hash(bytes(getpass('Guess: '), encoding='utf8'))
+    return g == p
