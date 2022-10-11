2e8b9b9ef05d313fd242b4afb6add206ee07c089
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-10-11 01:21:19 -0400

    Use secrets

--- src/digital_signatures.py
+++ src/digital_signatures.py
@@ -2,6 +2,7 @@
 
 from utils import *
 from elliptic_curve import *
+from secrets import SystemRandom, token_bytes
 
 # TODO
 # Basic hash-based digital signature
@@ -10,24 +11,29 @@ from elliptic_curve import *
 # - the curve
 # - public keys
 
-# public key from private key + curve
-def pk(sk, curve):
+# generate a key pair (sk, pk) for the curve
+def gen_ecc_pair(curve):
+    gen = SystemRandom()
     a, b, n, base = curve
     _, _, _, _, _, smult = make(a, b, n)
-    return smult(sk % n, base)
+    sk = gen.randint(2, n - 1)
+    return sk, smult(sk, base)
 
 # ECDSA
+# elliptic curve digital signature algorithm
 
+# sign
 def ecdsa_sign(msg, sk, curve):
     a, b, n, base = curve
     _, _, _, _, _, scalar_mult = make(a, b, n)
     _h = sha256(msg)
     h = int(str_to_bits(_h), base = 2) % n
-    k = int(str_to_bits(urandom(n)[16:]), base = 2) % n
+    k = int(str_to_bits(token_bytes(n)[16:]), base = 2) % n
     r, _ = scalar_mult(k, base)
     s = (inverse(k, n) * (h + r * int(sk))) % n
     return r, s
 
+# verify
 def ecdsa_verify(msg, sig, _pk, curve):
     rcvd_r, s = sig
     a, b, n, base = curve
@@ -38,7 +44,18 @@ def ecdsa_verify(msg, sig, _pk, curve):
     rx, _ = add(smult((h * c) % n, base), smult((rcvd_r * c) % n, _pk))
     return rcvd_r == rx
 
-# EdDSA
+# EdDSA (Ed25519)
+# Edward's twisted curve
+# - Curve25519
+# - SHA-512
+# - a = 0, b = 0
+# - p = 2**255 - 19
+# - base = (9, ...)
+# - base generates a cyclic subgroup of prime order 2**252 + 27742317777372353535851937790883648493
+
+# Curve25519 is given in Montgomery form
+# y**2 = x**3 + 4866612*x**2 + x (mod 2**255 - 19)
+curve25519 = (0, 0, 0, 0)
 
 # TODO
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
-import hashlib
-from os import urandom
 from Crypto.Cipher import AES
-# from Crypto import Random
+from secrets import token_bytes
 from base64 import b64encode, b64decode
 
 # AES
@@ -12,10 +10,14 @@ from base64 import b64encode, b64decode
 pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
+# generate key (default 32 bytes)
+def gen(*n: int):
+    if len(n) == 0: return token_bytes(32)
+    return token_bytes(n[0])
+
 # encryption
-def encrypt(pt):
-    key = urandom(32)
-    iv = urandom(16)
+def encrypt(pt, key):
+    iv = token_bytes(16)
     aes = AES.new(key, AES.MODE_CBC, iv)
     return key, b64encode(iv + aes.encrypt(pad(pt)))
 
@@ -26,27 +28,48 @@ def decrypt(_ct, key):
     aes = AES.new(key, AES.MODE_CBC, iv)
     return bytes.decode(unpad(aes.decrypt(ct[16:])))
 
+# make an encryption/decryption pair
+# - from a generated key
+# - from a supplied key
+def make(*k):
+    if len(k) == 0: key = gen()
+    else: key = k[0]
+    enc = lambda pt: encrypt(pt, key)
+    dec = lambda ct: decrypt(ct, key)
+    return key, enc, dec
+
 # SHA256
-# returns first 32 chars of hash as hex string
-def sha256(s, *_n: int):
-    n = _n[0]
-    if n < 0: raise ValueError('sha256 can only be applied a positive number of times')
+# returns first min(n, 32) chars of hash as hex string if n != 0 else 32
+def sha256(s: str, *_n: int) -> str:
+    if len(_n) == 0 or _n[0] < 10_000: n = 10_000
+    else: n = _n[0]
+    import hashlib
     m = hashlib.sha256()
-    m.update(bytes(str(s), 'utf-8'))
+    m.update(s.encode('utf-8'))
     if n == 0: return m.hexdigest()[32:]
     else: sha256(m.hexdigest(), n - 1)
 
+# SHA512
+# returns first min(n, 64) chars of hash as hex string if n != 0 else 64
+def sha512(s: str, *_n: int) -> str:
+    if len(_n) == 0 or _n[0] < 10_000: n = 10_000
+    else: n = _n[0]
+    import hashlib
+    m = hashlib.sha512()
+    m.update(s.encode('utf-8'))
+    if n == 0: return m.hexdigest()[64:]
+    else: sha512(m.hexdigest(), n - 1)
+
 # convert string to bits
 def str_to_bits(s):
-    res = [bin(byte)[2:] for byte in bytes(str(s), 'utf-8')]
-    return ''.join(res)
+    return ''.join([bin(byte)[2:] for byte in bytes(str(s), 'utf-8')])
 
 # xor
 # convert -> str -> bits
-# xor each bit
+# xor corresponding bits
 def xor(a, b):
     _a, _b = str(a), str(b)
     _a, _b = str_to_bits(_a), str_to_bits(_b)
-    res = [str(int(_a[i]) ^ int(_b[i])) for i in range(0, min(len(_a), len(_b)))]
-    return ''.join(res)
-
+    bitxor = lambda x, y, i: str(int(x[i]) ^ int(y[i]))
+    min_range = range(0, min(len(_a), len(_b)))
+    return ''.join([bitxor(_a, _b, i) for i in min_range])
