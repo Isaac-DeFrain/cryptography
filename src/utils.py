744170c627a926b5caa5ea7cbdbe132f282587cb
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-10-11 14:49:30 -0400

    Add types
    -str_to_bits inverse

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from Crypto.Cipher import AES
-from secrets import token_bytes
+from secrets import token_bytes, token_hex
 from base64 import b64encode, b64decode
 
 # AES
@@ -16,7 +16,7 @@ def gen(*n: int):
     return token_bytes(n[0])
 
 # encryption
-def encrypt(pt, key):
+def encrypt(pt: str, key: bytes):
     iv = token_bytes(16)
     aes = AES.new(key, AES.MODE_CBC, iv)
     return key, b64encode(iv + aes.encrypt(pad(pt)))
@@ -61,15 +61,59 @@ def sha512(s: str, *_n: int) -> str:
     else: sha512(m.hexdigest(), n - 1)
 
 # convert string to bits
-def str_to_bits(s):
-    return ''.join([bin(byte)[2:] for byte in bytes(str(s), 'utf-8')])
+def str_to_bits(s: str) -> str:
+    return ''.join([f'{byte:08b}' for byte in s.encode('utf-8')])
+
+# undo the conversion
+def bits_to_str(b: str) -> str:
+    group = False
+    count = 0
+    max_count = 1
+    rem = b
+    acc = []
+    while rem:
+        chk = rem[:8]
+        rem = rem[8:]
+        count += 1
+        if chk[0] == '0': group = False
+        if chk[:3] == '110':
+            group = True
+            max_count = 2
+        elif chk[:4] == '1110':
+            group = True
+            max_count = 3
+        elif chk[:5] == '11110':
+            group = True
+            max_count = 4
+            print(max_count)
+        while group:
+            if count > max_count: raise ValueError('UTF8 only has byte groups up to length 4')
+            elif chk[:2] == '10':
+                group = True
+                count += 1
+                chk += rem[:8]
+                rem = rem[8:]
+            else:
+                group = False
+                count = 0
+        acc.append(chk)
+    return ''.join([chr(int(chk, 2)) for chk in acc])
 
 # xor
 # convert -> str -> bits
 # xor corresponding bits
-def xor(a, b):
+def xor(a, b) -> str:
     _a, _b = str(a), str(b)
     _a, _b = str_to_bits(_a), str_to_bits(_b)
     bitxor = lambda x, y, i: str(int(x[i]) ^ int(y[i]))
     min_range = range(0, min(len(_a), len(_b)))
     return ''.join([bitxor(_a, _b, i) for i in min_range])
+
+# ------------------
+# --- unit tests ---
+# ------------------
+
+# 1000 random inverses
+for _ in range(1000):
+    x = token_hex(32)
+    assert(bits_to_str(str_to_bits(x)) == x)
