0c9ce85ecdf0539a68a01b5d1d71444c3d001ab0
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-10-26 19:16:20 -0400

    Add bits2str to bits

--- examples/bits/bits.py
+++ examples/bits/bits.py
@@ -2,15 +2,27 @@
 Bit manipulations
 """
 
+def bits2str(raw_bits: str) -> str:
+    diff = 8 - (len(raw_bits) % 8)
+    bits = '0' * diff + raw_bits
+    res = ''
+    while bits:
+        byte_bits = bits[:8]
+        bits = bits[8:]
+        res += chr(int(byte_bits, 2))
+    return res
+
 def str2bits(s: str) -> str:
-    """Convert string to bits"""
+    """
+    Convert string to bits
+    """
     return ''.join([f'{byte:08b}' for byte in s.encode('utf-8')])
 
 def xor(a: str, b: str) -> str:
-    """Bitwise xor"""
+    """
+    Bitwise xor
+    """
     a, b = str2bits(a), str2bits(b)
     bitxor = lambda x, y, i: str(int(x[i]) ^ int(y[i]))
     min_range = range(0, min(len(a), len(b)))
     return ''.join([bitxor(a, b, i) for i in min_range])
-
-__all__ = ["xor"]
--- examples/bits/test_bits.py
+++ examples/bits/test_bits.py
@@ -3,7 +3,7 @@ Bits unit tests
 """
 
 import unittest
-from bits import *
+from bits import xor
 from secrets import SystemRandom, token_hex
 
 class TestBits(unittest.TestCase):
@@ -13,8 +13,11 @@ class TestBits(unittest.TestCase):
             n = SystemRandom().randint(1, 100)
             x, y = token_hex(n), token_hex(n)
             res = xor(x, y)
-            assert(all(
-                [res[i] == '0' if x[i] == y[i] else res[i] == '1' for i in range(n)]))
+            for i in range(n):
+                # if ith chars are equal then the corresponding
+                # chunk of 8 bits should all be 0's
+                if x[i] == y[i]:
+                    assert(all([res[8 * i + j] == '0' for j in range(8)]))
 
 if __name__ == '__main__':
     unittest.main()
