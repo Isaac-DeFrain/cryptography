7f4ed97920508bb8e19eb23801c79fa9c281d9ad
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-11-02 01:29:55 -0400

    Add Montgomery form for elliptic curves

--- examples/elliptic_curve.py
+++ examples/elliptic_curve.py
@@ -6,7 +6,7 @@ Elliptic curves
 """
 
 from modular import inverse
-from typing import Annotated
+from typing import Annotated, Union
 
 class Point:
     """
@@ -29,27 +29,32 @@ class Weierstrass:
     modulus = Annotated[int, 'Modulus of Z/nZ']
 
     # coefficients of the Weierstrass form of the curve
-    a_coeff = Annotated[int, 'b coefficient of Weierstrass form']
-    b_coeff = Annotated[int, 'a coefficient of Weierstrass form']
+    a_coeff = Annotated[int, 'a coefficient of Weierstrass form']
+    b_coeff = Annotated[int, 'b coefficient of Weierstrass form']
 
     # ---------------
     # --- methods ---
     # ---------------
 
     def __init__(self, a: a_coeff, b: b_coeff, n: modulus):
-        """Initialize a Weierstrass curve"""
+        """
+        Initialize a Weierstrass curve
+        """
         self.weierstrass = a, b, n
 
     def f(self, x: int):
-        """Compute right side of Weierstrass equation"""
+        """
+        Compute right side of Weierstrass equation
+        """
         a, b, n = self.weierstrass
         return (x ** 3 + a * x + b) % n
 
     def check(self, p: Point) -> bool:
-        """Check that `p` is a point on the curve"""
-
+        """
+        Check that `p` is a point on the curve
+        """
         _, _, n = self.weierstrass
-        return p.y ** 2 % n == Weierstrass.f(self, p.x)
+        return p.y ** 2 % n == self.f(p.x)
 
     def points(self, *debug) -> list[Point]:
         """
@@ -67,17 +72,18 @@ class Weierstrass:
         for x in range(0, n):
             for y in range(0, n):
                 p = Point(x, y)
-                if Weierstrass.check(self, p):
+                if self.check(p):
                     pts.append(p)
         if debug:
-            print('+-----------------------------------------------')
+            print('+--------------------------------------------')
             print(f'+ Points of y^2 = x^3 + {a}x + {b} (mod {n})')
-            print('+-----------------------------------------------')
+            print('+--------------------------------------------')
             print(f'+ {pts.__len__()} points over Z/{n}Z')
-        return pts # TODO sort
+        return pts
 
     def points_n(self, k: int, lower: int = 0, *upper) -> list[Point]:
-        """Find k points on the curve given `lower` and/or `upper` bound(s)
+        """
+        Find k points on the curve given `lower` and/or `upper` bound(s)
 
         Includes the point at infinity, represented by (-1, -1)
         """
@@ -89,23 +95,24 @@ class Weierstrass:
         while count < k and lower < upper:
             for y in range(lower, upper):
                 p = Point(lower, y)
-                if Weierstrass.check(self, p):
+                if self.check(p):
                     count += 1
                     pts.append(p)
             lower += 1
         return pts
 
     def add(self, p: Point, q: Point) -> Point:
-        """Elliptic curve point addition"""
-
+        """
+        Elliptic curve point addition (Weierstrass form)
+        """
         a, _, n = self.weierstrass
         # coordinates
         x1, y1 = p.x, p.y
         x2, y2 = q.x, q.y
         # identity and inverse rules
         if x1 == x2 and y1 == -y2: return Point(-1, -1)
-        if p == (-1, -1): return q
-        if q == (-1, -1): return p
+        if p == Point(-1, -1): return q
+        if q == Point(-1, -1): return p
         # otherwise
         lam = ((3 * x1 ** 2 + a) * inverse(2 * y1, n)
             if p == q else (y2 - y1) * inverse(x2 - x1, n))
@@ -113,48 +120,92 @@ class Weierstrass:
         y3 = (lam * (x1 - x3) - y1) % n
         return Point(x3, y3)
 
-    def safe_add(self, p: Point, q: Point) -> 'Point | None':
+    def safe_add(self, p: Point, q: Point) -> Union[Point, None]:
         """
         Check that the points are on the curve before adding
         """
-        add = Weierstrass.add
-        check = Weierstrass.check
-        if check(self, p) and check(self, q):
-            return add(self, p, q)
+        if self.check(p) and self.check(q):
+            return self.add(p, q)
 
     def scalar_mult(self, k: int, p: Point) -> Point:
-        """Scalar multiplication"""
-
+        """
+        Scalar multiplication
+        """
         if k == 0: return Point(-1, -1)
         if k == 1: return p
         if k > 1:
-            add = Weierstrass.add
-            mult = Weierstrass.scalar_mult
-            return add(self, p, mult(self, k - 1, p))
+            return self.add(p, self.scalar_mult(k - 1, p))
         else:
             raise ValueError(f'Expect element of Z/nZ, got: {k}')
 
 class Montgomery:
-    """Montgomery form of an elliptic curve"""
+    """
+    Montgomery form of an elliptic curve
+    """
 
-    def __init__(self):
-        """Initialize a new Montgomery"""
-        self.montgomery = 0, 0, 0
+    # --------------------
+    # --- type aliases ---
+    # --------------------
 
-    # TODO
+    # modulus of the underlying field Z/nZ
+    modulus = Annotated[int, 'Modulus of Z/nZ']
+
+    # coefficients of the Montgomery form of the curve
+    a_coeff = Annotated[int, 'a coefficient of Montgomery form']
+    b_coeff = Annotated[int, 'b coefficient of Montgomery form']
+
+    # ---------------
+    # --- methods ---
+    # ---------------
+
+    def __init__(self, a: a_coeff, b: b_coeff, n: modulus):
+        """
+        Initialize a new Montgomery
+        """
+        self.montgomery = a, b, n
+
     def f(self, x: int) -> int:
-        """Compute right hand side of Montgomery equation"""
-        return x
+        """
+        Compute right side of Montgomery equation
+        """
+        a, _, n = self.montgomery
+        return (x ** 3 + a * x ** 2 + x) % n
 
     def check(self, p: Point) -> bool:
-        """Check that `p` is a point on the curve"""
+        """
+        Check that `p` is a point on the curve
+        """
+        _, b, n = self.montgomery
+        return b * p.y ** 2 % n == self.f(p.x)
 
-        _, _, n = self.montgomery
-        # TODO
-        return p.y ** 2 % n == self.f(p.x)
+    def points(self, *debug) -> list[Point]:
+        """
+        Return the list of all points on the elliptic
+        curve over Z/nZ with Montgomery form:
+
+            `b*y^2 = x^3 + a*x^2 + x`
+
+        Includes the point at infinity, represented by (-1, -1)
+
+        Inefficient implementation: use only on small fields
+        """
+        a, b, n = self.montgomery
+        pts = [Point(-1, -1)]
+        for x in range(0, n):
+            for y in range(0, n):
+                p = Point(x, y)
+                if self.check(p):
+                    pts.append(p)
+        if debug:
+            print('+-----------------------------------------------')
+            print(f'+ Points of {b}y^2 = x^3 + {a}x^2 + x (mod {n})')
+            print('+-----------------------------------------------')
+            print(f'+ {pts.__len__()} points over Z/{n}Z')
+        return pts
 
     def points_n(self, k: int, lower: int = 0, *upper) -> list[Point]:
-        """Find k points on the curve given `lower` and/or `upper` bound(s)
+        """
+        Find k points on the curve given `lower` and/or `upper` bound(s)
 
         Includes the point at infinity, represented by (-1, -1)
         """
@@ -171,3 +222,46 @@ class Montgomery:
                     pts.append(p)
             lower += 1
         return pts
+
+    def add(self, p: Point, q: Point) -> Point:
+        """
+        Elliptic curve point addition (Montgomery form)
+        """
+        a, b, n = self.montgomery
+        # coordinates
+        x1, y1 = p.x, p.y
+        x2, y2 = q.x, q.y
+        # identity and inverse rules
+        if x1 == x2 and y1 == -y2: return Point(-1, -1)
+        if p == Point(-1, -1): return q
+        if q == Point(-1, -1): return p
+        # otherwise
+        m = (y2 - y1) * inverse(x2 - x1, n) % n
+        if p != q:
+            x3 = (b * m ** 2 - a - x1 - x2) % n
+            y3 = (m * (2 * x1 + x2 + a) - b * m ** 3 - y1) % n
+        else:
+            l = (3 * x1 ** 2 + 2 * a * x1 + 1) * inverse(2 * b * y1, n) % n
+            x3 = (b * l ** 2 - a - 2 * x1) % n
+            y3 = ((3 * x1 + a) * l - b * l ** 3 - y1) % n
+        return Point(x3, y3)
+
+    def safe_add(self, p: Point, q: Point) -> Union[Point, None]:
+        """
+        Check that the points are on the curve before adding
+        """
+        if self.check(p) and self.check(q):
+            return self.add(p, q)
+
+    def scalar_mult(self, k: int, p: Point) -> Point:
+        """
+        Scalar multiplication
+        """
+        if k == 0: return Point(-1, -1)
+        if k == 1: return p
+        if k > 1:
+            return self.add(p, self.scalar_mult(k - 1, p))
+        else:
+            raise ValueError(f'Expect element of Z/nZ, got: {k}')
+
+# TODO conversion between forms
--- notes/src/elliptic_curve_crypto.md
+++ notes/src/elliptic_curve_crypto.md
@@ -2,6 +2,8 @@
 
 Increased security over RSA
 
+Not quantum secure!
+
 ## Geometric foundations
 
 TODO
@@ -9,3 +11,15 @@ TODO
 ## Elliptic curves over finite fields
 
 TODO
+
+### Weierstrass form
+
+```
+W_ab: y^2 = x^3 + a*x + b
+```
+
+### Montgomery form
+
+```
+M_ab: b*y^2 = x^3 + a*x^2 + x
+```
