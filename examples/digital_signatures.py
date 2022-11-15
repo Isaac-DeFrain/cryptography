c6324c72f341d738ab515aac561f6177797d30e0
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-11-15 12:42:28 -0500

    Fix digital signatures code

--- examples/digital_signatures.py
+++ examples/digital_signatures.py
@@ -3,7 +3,7 @@ Digital signatures
 """
 
 from utils import *
-from modular import inverse
+from modular import Mod
 from hashlib import sha256
 from elliptic_curve import Weierstrass, Montgomery, Point
 from secrets import SystemRandom, token_hex
@@ -20,7 +20,7 @@ curve = Annotated[
     tuple[int, int, int, tuple[int, int]],
     'Coefficients, modulus, and base point']
 
-class BasedECC:
+class BasedEC:
     """A elliptic curve and a base point
 
     - `curve`
@@ -31,92 +31,97 @@ class BasedECC:
     # - Weierstrass
     #   - check discriminant
     def __init__(self, curve: Union[Montgomery, Weierstrass], base: Point):
-        self.base = None
+        self.base = Point(-1, -1)
         if type(curve) == Weierstrass:
-            if Weierstrass.check(curve, base):
+            if curve.check(base):
                 self.base = base
             else:
-                try:
-                    self.base = Weierstrass.points_n(1)[1]
-                except ValueError: ()
+                self.base = curve.points_n(1)[1]
             self.curve = curve
         if type(curve) == Montgomery:
-            if Montgomery.check(curve, base):
+            if curve.check(base):
                 self.base = base
                 self.curve = curve
             else:
-                x = SystemRandom.randint(1, n)
+                x = SystemRandom().randint(1, n)
                 checked = []
                 while x not in checked:
-                    try:
-                        self.base = Montgomery.points_n(1, x)[1]
-                    except ValueError: ()
-
+                    self.base = curve.points_n(1, x)[1]
                     checked.append(x)
                     x = (x + 1) % n
-                if self.base == None:
+                if self.base == Point(-1, -1):
                     raise ValueError(f'Only point at infinity... something is wrong with {curve, type(curve)}')
 
 class Key:
-    """Elliptic curve key pair generation"""
+    """
+    Elliptic curve key pair generation
+    """
 
-    def gen(self, bc: BasedECC):
-        """Generate a key pair for the curve"""
-
+    def gen(self, bc: BasedEC):
+        """
+        Generate a key pair for the curve
+        """
         gen = SystemRandom()
-        a, b, n, base = bc.curve.weierstrass, bc.base
+        a, b, n = bc.curve.form
+        base = bc.base
         curve = Weierstrass(a, b, n)
         sk = gen.randint(2, n - 1)
         return sk, curve.scalar_mult(sk, base)
 
-    def __init__(self, bc: BasedECC):
+    def __init__(self, bc: BasedEC):
         sk, pk = Key.gen(self, bc)
         self.sk = sk
         self.pk = pk
 
 class ECDSA:
-    """Elliptic Curve Digital Signature Algorithm"""
+    """
+    Elliptic Curve Digital Signature Algorithm
+    """
 
-    def __init__(self, curve: BasedECC, key: Key, hash = sha256()):
+    def __init__(self, curve: BasedEC, key: Key, hash = sha256()):
         self.key = key
         self.hash = hash
         self.base = curve.base
         self.curve = curve.curve
 
     class Signature:
-        """ECDSA signature"""
-
+        """
+        ECDSA signature
+        """
         def __init__(self, x: int, s: int):
             self.x = x
             self.s = s
 
     # sign
     def sign(self, msg: str) -> Signature:
-        a, b, n = self.curve.weierstrass
-        curve = Weierstrass(a, b, n)
+        a, b, n = self.curve.form
+        if type(self.curve) == Weierstrass:
+            curve = Weierstrass(a, b, n)
+        else:
+            curve = Montgomery(a, b, n)
         m = self.hash
         m.update(msg.encode('utf-8'))
         h = int(m.hexdigest(), 16) % n
         k = int(token_hex(32), 16) % n
-        x, _ = curve.scalar_mult(k, self.base)
-        s = (inverse(k, n) * (h + x * self.key.sk)) % n
+        x = curve.scalar_mult(k, self.base).x
+        s = (Mod(n).inverse(k) * (h + x * self.key.sk)) % n
         return ECDSA.Signature(x, s)
 
     def sign_msg(self, msg: str) -> tuple[str, Signature]:
         return msg, ECDSA.sign(self, msg)
 
     # verify
-    def verify(self, msg: str, sig: Signature, curve) -> bool:
-        x, s = sig.x, sig.x
-        a, b, n, base = curve
-        curve = Weierstrass(a, b, n)
-        m = sha256()
+    def verify(self, msg: str, sig: Signature) -> bool:
+        x, s = sig.x, sig.s
+        curve = self.curve
+        a, b, n = curve.form
+        base = self.base
+        m = self.hash
         m.update(msg.encode('utf-8'))
         h = int(m.hexdigest(), 16) % n
-        c = inverse(s, n)
-        add = Weierstrass.add
-        smult = Weierstrass.scalar_mult
-        rx, _ = add(smult((h * c) % n, base), smult((x * c) % n, self.key.pk))
+        c = Mod(n).inverse(s)
+        add, smult = curve.add, curve.scalar_mult
+        rx = add(smult((h * c) % n, base), smult((x * c) % n, self.key.pk)).x
         return x == rx
 
 # EdDSA (Ed25519)
